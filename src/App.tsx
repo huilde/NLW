6a9800482fee2774b7deca288ba537220a8a60d1
huilde <huilde.viana@venturus.org.br>
2022-05-05 11:16:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { Widget } from './components/widget'
+import "./global.css"
+function App() {
+  return (
+   <Widget></Widget>
+  )
+}
+
+export default App
